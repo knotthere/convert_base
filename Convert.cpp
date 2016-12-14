7615dbd095054133d9dcd4a12b5d2323d86a4eac
knottl <larry.knott@autodesk.com>
2016-12-14 10:52:38 -0800

    Show more results

--- Convert.cpp
+++ Convert.cpp
@@ -6,71 +6,81 @@
 
 #include "iostream"
 #include "string"
-#include <cassert>	// assert
+#include <cassert>    // assert
 #include <map>
-#include <functional>
+#include <stdlib.h>
+
 using namespace std;
 
 // Propmpt the user for an Int.
 bool getInt(string strPrompt, int& nInput)
 {
-	string strInput;
-	cout << strPrompt;
-	getline(cin, strInput);
-	if ("" == strInput) {
-		return false;	// All done
-	} else {
-		// Get the c style pointer to the string array
-		const char* pStr = strInput.c_str();
-		// Convert that to an integer
-		// https://msdn.microsoft.com/en-us/library/yd5xkb5c.aspx
-		nInput = atoi(pStr);
-		return true;
-	}
+    string strInput;
+    cout << strPrompt;
+    getline(cin, strInput);
+    if ("" == strInput) {
+        return false;    // All done
+    } else {
+        // Get the c style pointer to the string array
+        const char* pStr = strInput.c_str();
+        // Convert that to an integer
+        // https://msdn.microsoft.com/en-us/library/yd5xkb5c.aspx
+        nInput = atoi(pStr);
+        return true;
+    }
 }
 
 // Convert a base 10 integer (one that uses ten characters per digit) to another base, such
 // as base 16.
 int Convert()
 {
-	int 	nBase = 16,		// What base do you want to conver to?
-			nInput = 0;
+    // Use the string array for mapping from decimal to the new base (up to 36)
+    const string strMap = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	// Use the string array for mapping from decimal to the new base
-	string strMap = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Ask for the new base
+    int nBase = 0;  // What base do you want to conver to?
+    if (!getInt("Convert base 10 integers to this base: ", nBase)) {
+        return 0;   // We are done
+    } else if ((nBase < 1) || (nBase > 36)) {
+        cout << "Target base must be greater than 0 and less than 37.";
+        return 1;   // Error condition
+    }
 
-	// Ask for the new base
-	if (!getInt("Convert base 10 integers to this base: ", nBase)) {
-		return 0;	// We are done
-	}
+    int nInput = 0;
+    while (getInt("Integer to convert: ", nInput)) {
+        // Let's show 5 numbers around the one entered, 2 before and 2 after...
+        int nBegInput = nInput - 2,
+            nEndInput = nInput + 2;
+        for (int nCurInput = nBegInput; nCurInput <= nEndInput; nCurInput++) {
+            // Now, iterate over the integer, and collect each right-most remainder in base format
+            string strOutput;
+            int nWorkingInput = nCurInput;  // Use this variable for computations
+            do {
+                int nRemainder = nWorkingInput % nBase;
+                // Simply assert what we expect...
+                //assert((nRemainder >= 0) && (nRemainder < nBase));
 
-	while (getInt("Integer to convert: ", nInput)) {
-		// Now, iterate over the passed integer, and collect each right-most remainder in base format
-		string strOutput;
-		do {
-			int nRemainder = nInput % nBase;
-			// Simply assert what we expect...
-			assert((nRemainder >= 0) && (nRemainder < nBase));
+                // Get the character at this indicie in our array
+                const char chDigit = strMap[nRemainder];
+                // and pre-pend it to out output string
+                strOutput = chDigit + strOutput;
 
-			// Get the character at this indicie in our array
-			const char chDigit = strMap[nRemainder];
-			// and pre-pend it to out output string
-			strOutput = chDigit + strOutput;
+                // Reset out input variable, for the next digit
+                nWorkingInput = (int)(nWorkingInput / nBase);
+            } while (nWorkingInput > 0);
 
-			// Reset out input variable, for the next digit
-			nInput = (int)(nInput / nBase);
-		} while (nInput > 0);
+            cout << to_string(nCurInput) + ": ";
+            cout << strOutput;
+            cout << "\n";
+        }
+    }
 
-		cout << strOutput;
-		cout << "\n";
-	}
-
-	return 0;
+    return 0;   // Success condition
 }
 
 
 int main(int argc, const char * argv[])
 {
-	Convert();
+    Convert();
 }
 
--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # convert_base
-C++ MS Code Project to convert from Base 10
+C++ MS Code Project to convert integers from base (radix) 10 to another base.
+
+[Radix - Wikipedia](https://en.wikipedia.org/wiki/Radix)
 
 Sample output:
 
+	larry:~/Documents/Philip/convert_base (master=)$ ./a.out
+	Convert base 10 integers to this base: 2
 	Integer to convert: 10
-	A
+	1010
+	Integer to convert: 11
+	1011
+	Integer to convert: 1
+	1
+	Integer to convert: 2
+	10
+	Integer to convert: 3
+	11
+	Integer to convert: 4
+	100
+	Integer to convert:
+	larry:~/Documents/Philip/convert_base (master=)$ ./a.out
+	Convert base 10 integers to this base: 16
 	Integer to convert: 9
 	9
+	Integer to convert: 10
+	A
 	Integer to convert: 15
 	F
 	Integer to convert: 16
@@ -15,8 +34,6 @@ Sample output:
 	FF
 	Integer to convert: 256
 	100
-	Integer to convert: 
-
 
 
 
